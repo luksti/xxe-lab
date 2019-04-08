f73727dd3d94dd9bd93e33e2d1c8b388b9829f92
Lauri Yksti <lauri.yksti@icefire.ee>
2019-04-08 13:13:57 +0300

    spring boot application to scan all base packages

--- src/main/java/xxe/Launcher.java
+++ src/main/java/xxe/Launcher.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.WebApplicationInitializer;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"xxe"})
 public class Launcher extends SpringBootServletInitializer implements WebApplicationInitializer {
 
   @Override
